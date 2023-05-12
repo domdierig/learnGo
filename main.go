dfe4b262429e749232a8f2765b3741c196d44bb6
Dominic Dierig <dominic.dierig@outlook.de>
2023-05-12 22:19:13 +0200

    stuff

--- main.go
+++ main.go
@@ -1,14 +1,10 @@
 package main
 
 import (
-	"github.com/domdierig/learnGo/adventOfCode2022/day4"
+	"github.com/domdierig/learnGo/refVsValue"
 )
 
-func main() {
-	// refVsValue.RefVsValue()
-	// day1.CalcCalories()
-	// day2.CalcScore()
-	// day3.CalcPrio()
 
-	day4.CalcOverlappingSections()
+func main() {
+	refVsValue.RefVsValue()
 }
